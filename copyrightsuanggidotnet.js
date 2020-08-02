b5830707241231ddffdd5138b86757c13ae74eb8
Suanggi Survival Papua <55645633+SuanggiSurvivalPapua@users.noreply.github.com>
2020-08-03 05:11:48 +0900

    Create copyrightsuanggidotnet.js

---
+++ copyrightsuanggidotnet.js
@@ -0,0 +1,4 @@
+var element = document.getElementById("copyrightsuanggidotnet");
+ if (element.getAttribute("href") != "https://www.suanggi.net") {
+   window.location.href = "https://www.suanggi.net";
+}
